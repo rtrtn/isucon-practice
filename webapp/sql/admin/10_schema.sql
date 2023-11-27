1850828a0e95234807d7180c64ccea97d3e6d44a
rtrtn <rtro1407@gmail.com>
2023-11-27 23:08:19 +0900

    visit_historyにインデックス追加 731

--- webapp/sql/admin/10_schema.sql
+++ webapp/sql/admin/10_schema.sql
@@ -1,7 +1,9 @@
 USE `isuports`;
 
 DROP TABLE IF EXISTS `tenant`;
+
 DROP TABLE IF EXISTS `id_generator`;
+
 DROP TABLE IF EXISTS `visit_history`;
 
 CREATE TABLE `tenant` (
@@ -12,14 +14,14 @@ CREATE TABLE `tenant` (
   `updated_at` BIGINT NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
 CREATE TABLE `id_generator` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `stub` CHAR(1) NOT NULL DEFAULT '',
-  PRIMARY KEY  (`id`),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `stub` (`stub`)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
 
 CREATE TABLE `visit_history` (
   `player_id` VARCHAR(255) NOT NULL,
@@ -27,5 +29,6 @@ CREATE TABLE `visit_history` (
   `competition_id` VARCHAR(255) NOT NULL,
   `created_at` BIGINT NOT NULL,
   `updated_at` BIGINT NOT NULL,
-  INDEX `tenant_id_idx` (`tenant_id`)
-) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
+  INDEX `tenant_id_idx` (`tenant_id`),
+  INDEX `tenant_competition_player_idx`(`tenant_id`, `competition_id`, `player_id`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8mb4;
